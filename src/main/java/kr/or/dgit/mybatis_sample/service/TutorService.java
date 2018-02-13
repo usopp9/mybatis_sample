06c93841111f0ab7f7af7a8f1c17d62944123e4e
usopp <usopp9@naver.com>
2018-02-13 10:34:17 +0900

    tutor ,course add

---
+++ src/main/java/kr/or/dgit/mybatis_sample/service/TutorService.java
@@ -0,0 +1,23 @@
+package kr.or.dgit.mybatis_sample.service;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.mybatis_sample.dao.TutorDao;
+import kr.or.dgit.mybatis_sample.dto.Tutor;
+import kr.or.dgit.mybatis_sample.util.MybatisSqlSessionFactory;
+
+public class TutorService {
+	private static final Log log = LogFactory.getLog(StudentService.class);
+	private String namespace = "kr.or.dgit.mybatis_sample.dao.TutorDao.";
+	
+	
+	public Tutor findTutorByTutorId(Tutor tutor) {
+		log.debug("findTutorByTutorId()");
+		try (SqlSession sqlSession = MybatisSqlSessionFactory.openSession();) {
+			TutorDao tutorDao = sqlSession.getMapper(TutorDao.class);
+			return tutorDao.selectTutorByTutorId(tutor);
+		}
+	}
+}
